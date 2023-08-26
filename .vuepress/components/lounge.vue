661813607490516f9565eba30b82aacc766e6611
Erisa A <erisa@erisa.uk>
2023-08-26 19:52:40 +0100

    Don't gitignore components

---
+++ .vuepress/components/lounge.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Lounge names history</h1>
+    <em>List is curated by Torch</em>
+    <hr>
+    <div id="loungeNames">
+        <h2>Loading...</h2>
+    </div>
+</template>    
+
+<style :is="'style'">
+.theme-default-content:not(.custom) {
+    max-width: max-content !important;
+}
+</style>
+
+<script>
+fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://torch.is/typing/loungenameshtml.txt')}`)
+    .then(response => {
+        if (response.ok) return response.json()
+        throw new Error('Network response was not ok.')
+    })
+    .then(data => document.getElementById('loungeNames').innerHTML = data.contents);
+</script>
