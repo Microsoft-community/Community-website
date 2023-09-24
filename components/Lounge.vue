0a1119bbe028e87ab9628da763c401632e4045b0
itsWindows11 <itswin11@outlook.com>
2023-09-24 17:35:36 +0300

    Made lounge page use full width
    Also fixed client only-code running during build

--- components/Lounge.vue
+++ components/Lounge.vue
@@ -1,23 +1,25 @@
 <template>
     <h1>Lounge names history</h1>
-    <em>List is curated by Torch</em>
+    <em>List is curated by <a href="https://github.com/torchgm">Torch</a></em>
     <hr>
     <div id="loungeNames">
         <h2>Loading...</h2>
     </div>
-</template>    
-
-<style :is="'style'">
-.theme-default-content:not(.custom) {
-    max-width: max-content !important;
-}
-</style>
+</template>
 
 <script>
-fetch(`https://torch.is/typing/loungenameshtml.txt`)
-    .then(response => {
-        if (response.ok) return response.text()
-        throw new Error('Network response was not ok.')
-    })
-    .then(data => document.getElementById('loungeNames').innerHTML = data);
+import { onMounted } from "vue";
+
+onMounted(() => {
+    if (document !== undefined) {
+        document.title = "Microsoft Community";
+
+        fetch(`https://torch.is/typing/loungenameshtml.txt`)
+            .then(response => {
+                if (response.ok) return response.text()
+                throw new Error('Network response was not ok.')
+            })
+            .then(data => document.getElementById('loungeNames').innerHTML = data);
+    }
+});
 </script>
--- member/lounge.md
+++ member/lounge.md
@@ -1,6 +1,19 @@
 ---
 sidebar: false
+footer: false
+aside: false
+pageClass: lounge-page
 ---
+
+<style>
+div.lounge-page > .VPContent > .VPDoc:not(.has-sidebar) > .container > .content {
+    max-width: max-content !important;
+}
+
+div.lounge-page > .VPContent > .VPDoc:not(.has-sidebar) > .container {
+    max-width: max-content !important;
+}
+</style>
 
 <ClientOnly>
     <Lounge />
