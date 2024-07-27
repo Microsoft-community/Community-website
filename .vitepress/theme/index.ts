da23f2b1cbd404cc4f16a82a816ab6f008b6c5b9
Erisa A <erisa@erisa.uk>
2024-07-27 03:53:09 +0100

    Add medium-zoom

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,9 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+import mediumZoom from 'medium-zoom';
 import Lounge from '../../src/components/Lounge.vue'
 import './style.css'
 
@@ -14,5 +17,19 @@ export default {
   enhanceApp({ app, router, siteData }) {
     // ...
     app.component('Lounge', Lounge);
-  }
+  },
+
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
 }
