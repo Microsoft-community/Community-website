d075d6d7f8cdc5f1e51a3a03549eb2cbb18fb51f
Andy Nguyen <icorei783@gmail.com>
2024-08-20 01:29:51 +0700

    Create VanityCollapsible.vue

---
+++ src/components/VanityCollapsible.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="collapsible">
+    <button @click="isOpen = !isOpen">{{ title }}</button>
+    <div v-show="isOpen" class="content">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
